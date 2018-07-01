673fe5bdab9809549693e08446934f5481a761b9
Ivan <Zolotukhin.ivan.1@gmail.com>
2018-07-01 17:07:04 +0300

    Beginning of work
    Added html, js, css, and preloader.

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+
+jQuery(document).ready(function($) {
+    $(window).load(function() {
+        setTimeout(function() {
+            $('#preloader').fadeOut('slow', function() {});
+        }, 2000);
+
+    });
+});
