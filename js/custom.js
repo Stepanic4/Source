810e416d62497dff1a6cc3f01cde47b3c426ea12
Ivan <Zolotukhin.ivan.1@gmail.com>
2018-07-01 17:17:35 +0300

    add margin
    added margin

--- js/custom.js
+++ js/custom.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function($) {
     $(window).load(function() {
         setTimeout(function() {
             $('#preloader').fadeOut('slow', function() {});
-        }, 2000);
+        }, 1000);
 
     });
 });
