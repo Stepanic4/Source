a2e1e4b050912e3785198aa10fee681af336a855
Ivan <Zolotukhin.ivan.1@gmail.com>
2018-07-02 21:44:58 +0300

    Added page
    Added new html and scroll

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,5 @@
 
+// ==============================Preloader===========================
 jQuery(document).ready(function($) {
     $(window).load(function() {
         setTimeout(function() {
@@ -7,7 +8,6 @@ jQuery(document).ready(function($) {
 
     });
 });
-
 
 var M = Math,
     PI = M.PI,
@@ -64,3 +64,55 @@ ctx.translate( cx, cy );
 })();
 
 document.getElementById('preloader').appendChild( canvas );
+// ============================END Preloader=========================
+$(document).ready(function() {
+
+    var curPage = 1;
+    var numOfPages = $(".skw-page").length;
+    var animTime = 700;
+    var scrolling = false;
+    var pgPrefix = ".skw-page-";
+
+    function pagination() {
+        scrolling = true;
+
+        $(pgPrefix + curPage).removeClass("inactive").addClass("active");
+        $(pgPrefix + (curPage - 1)).addClass("inactive");
+        $(pgPrefix + (curPage + 1)).removeClass("active");
+
+        setTimeout(function() {
+            scrolling = false;
+        }, animTime);
+    };
+
+    function navigateUp() {
+        if (curPage === 1) return;
+        curPage--;
+        pagination();
+    };
+
+    function navigateDown() {
+        if (curPage === numOfPages) return;
+        curPage++;
+        pagination();
+    };
+
+    $(document).on("mousewheel DOMMouseScroll", function(e) {
+        if (scrolling) return;
+        if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
+            navigateUp();
+        } else {
+            navigateDown();
+        }
+    });
+
+    $(document).on("keydown", function(e) {
+        if (scrolling) return;
+        if (e.which === 38) {
+            navigateUp();
+        } else if (e.which === 40) {
+            navigateDown();
+        }
+    });
+
+});
